5f6ddb5b73cca1f5491ece62fc88b585e79fd3c9
Charlie <n7best@gmail.com>
2017-05-21 18:09:10 +0000

    intro and footer template

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
 $(document).ready( function(){
+
+    //menu
     $('#toggle').click(function() {
        $(this).toggleClass('active');
        $('#overlay').toggleClass('open');
     });
+
+    //scroll
+    window.sr = ScrollReveal();
+    sr.reveal('.reveal', {
+        duration: 1000,
+        scale: 1,
+        opacity: 0.2,
+        distance: '15px',
+        reset: true,
+    }, 250);
 })
