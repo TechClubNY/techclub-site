3cc26cb6da676166eed42ad476d3f479d1f67d46
Charlie <n7best@gmail.com>
2017-05-22 00:54:52 +0000

    preloader

--- js/main.js
+++ js/main.js
@@ -1,21 +1,27 @@
 $(document).ready( function(){
+    //preloader
+    Pace.on("done", function(){
+        $(".cover").fadeOut(500);
 
+        //scroll
+        window.sr = ScrollReveal();
+        sr.reveal('.reveal', {
+            duration: 1000,
+            scale: 1,
+            opacity: 0.2,
+            distance: '15px',
+            reset: true,
+            delay: 250
+        }, 250);
+
+
+    });
     //menu
     $('#toggle').click(function() {
        $(this).toggleClass('active');
        $('#overlay').toggleClass('open');
     });
 
-    //scroll
-    window.sr = ScrollReveal();
-    sr.reveal('.reveal', {
-        duration: 1000,
-        scale: 1,
-        opacity: 0.2,
-        distance: '15px',
-        reset: true,
-        container: '.statistic'
-    }, 250);
 
     //atvimg
     atvImg();
